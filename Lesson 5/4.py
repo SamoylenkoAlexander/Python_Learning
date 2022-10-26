f20ea9d91c854afaba1f90f712a9b182250b8cbc
Samoylenko <samoylenk0.alexander@yandex.ru>
2022-10-26 20:49:26 +0300

    Lesson 5 done-ish

--- Lesson 5/4.py
+++ Lesson 5/4.py
@@ -1,33 +1,35 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+with open('input_data.txt', 'w', encoding='UTF-8') as file:
+    file.write(input('Напишите текст начальный текст, который необходимо сжать: '))
+with open('input_data.txt', 'r') as data:
+    string = data.readline()
+
+print (string)
+
+def data_encryption(txt):
+    encond = ''
+    prev_char = ''
+    count = 1
+    if not txt:
+        return ''
+
+    for char in txt:
+        if char != prev_char:
+            if prev_char:
+                encond += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encond += str(count) + prev_char
+        return encond
 
 
+secret = data_encryption(string)
 
-
-# def encryption(txt):
-#     count = 1
-#     res = ''
-#     for i in range(len(txt)-1):
-#         if txt[i] == txt[i+1]:
-#             count += 1
-#         else:
-#             res = res + str(count) + txt[i]
-#             count = 1
-#     if count > 1 or (txt[len(txt)-2] != txt[-1]):
-#         res = res + str(count) + txt[-1]
-#     return res
-
-# def decryption(txt):
-#     number = ''
-#     res = ''
-#     for i in range(len(txt)):
-#         if not txt[i].isalpha():
-#             number += txt[i]
-#         else:
-#             res = res + txt[i] * int(number)
-#             number = ''
-#     return res
-# s = input("Введите текст для кодировки: ")
-# print(f"Текст после кодировки: {encryption(s)}")
-# print(f"Текст после дешифровки: {decryption(encryption(s))}")
+with open('output_data.txt', 'w', encoding='UTF-8') as file:
+    file.write(f'{secret}')
+print(secret)
